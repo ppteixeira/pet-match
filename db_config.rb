174d291ccf31571e668d952b6fdc68d777171960
Pedro Araujo <ppteixeira@outlook.com>
2016-11-04 11:55:12 +1100

    Bundle install - heroku

--- db_config.rb
+++ db_config.rb
@@ -5,4 +5,5 @@ options = {
   database: 'petmatch'
 }
 
-ActiveRecord::Base.establish_connection(options)
+# ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
